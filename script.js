1ae9a9f4069e142291bb7a454f65199dd1b94ec4
Hrumo <137638658+hrumo@users.noreply.github.com>
2026-08-15 17:34:39 -0300

    Update script.js

--- script.js
+++ script.js
@@ -144,8 +144,12 @@ function formatDate(date) {
 
 function formatDateTime(date) {
   return new Intl.DateTimeFormat("pt-BR", {
-    day: "2-digit", month: "2-digit", year: "numeric",
-    hour: "2-digit", minute: "2-digit"
+    day: "2-digit",
+    month: "2-digit",
+    year: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+    timeZone: "America/Sao_Paulo"
   }).format(date);
 }
 
